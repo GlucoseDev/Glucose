dbc7ad0e2a181e269d4e813a30d6bca2dde4c178
MrSlimeDiamond <findlayrichardson3@gmail.com>
2021-06-07 12:10:39 +1200

    script to decompile - credits to LoomDev!

---
+++ tools/scripts/remap_decompile.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Credit to LoomDev for providing most of this script
+
+mcVersion="1.16.5"
+
+if [ $mcVersion == "1.16.5" ]; then
+	mkdir .cache
+	curl -L https://launcher.mojang.com/v1/objects/41285beda6d251d190f2bf33beadd4fee187df7a/server.txt > .cache/server.txt 
+fi
+
+printf "Converting $mcVersion Minecraft mappings..."
+java -cp "tools/jars/enigma/*" "cuchaz.enigma.command.Main" convert-mappings proguard ".cache/$mcVersion/server.txt" tinyv2:obf:deobf ".cache/$mcVersion/server.tiny" || exit 1
+printf " Done!\n"
+ 
+printf "Mapping $mcVersion Minecraft server jar...\n"
+java -jar "tools/jars/tiny-remapper-0.3.1.72-fat.jar" ".cache/$mcVersion/server.jar" ".cache/$mcVersion/server-deobf.jar" ".cache/$mcVersion/server.tiny" obf deobf --renameInvalidLocals || exit 1
+
+printf "Installing $mcVersion mapped Minecraft server jar in your local maven repo..."
+mvn install:install-file -Dfile=".cache/$mcVersion/server-deobf.jar" -DgroupId="org.loomdev" -DartifactId="minecraft-server" -Dversion="$mcVersion-SNAPSHOT" -Dpackaging="jar" > /dev/null
+printf " Done!\n"
+
+printf "Extracting $mcVersion mapped Minecraft server jar..."
+unzip -o ".cache/$mcVersion/server-deobf.jar" "net/**/*" -d ".cache/$mcVersion/extracted/" > /dev/null
+printf " Done!\n"
+
+printf "Decompiling $mcVersion mapped Minecraft source...\n"
+mkdir -p ".cache/$mcVersion/decompiled/"
+java -jar "tools/jars/fernflower.jar" -dgs=1 -hdc=0 -rbr=0 -asc=1 -udv=0 ".cache/$mcVersion/extracted/" ".cache/$mcVersion/decompiled/"
+printf "Done!\n"
