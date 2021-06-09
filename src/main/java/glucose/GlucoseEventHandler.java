b448bcf3284544dee28e3af92f6a2712dbb95bcb
MrSlimeDiamond <findlayrichardson3@gmail.com>
2021-06-09 16:23:36 +1200

    some fixes

--- src/main/java/glucose/GlucoseEventHandler.java
+++ src/main/java/glucose/GlucoseEventHandler.java
@@ -3,11 +3,8 @@ package glucose;
 import java.io.File;
 import java.io.IOException;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 /* Ty Mojang.. */
 public class GlucoseEventHandler {
-    private static final Logger LOGGER = LogManager.getLogger();
     public void serverStartEvent(){
         /* Load plugins */
         final File PLUGIN_DIR = new File(System.getProperty("java.io.tmpdir"));
@@ -16,13 +13,8 @@ public class GlucoseEventHandler {
             /* Ignore */
         } else {
             /* Create plugins directory if it doesn't exist */
-            LOGGER.info("Plugins directory does not exist, creating...");
-            try {
-                plugDir.mkdir();
-            } catch(IOException e) {
-                LOGGER.info("Could not create plugins directory! Stack trace:");
-                e.printStackTrace();
-            }
+            Logger.info("Plugins directory does not exist, creating...");
+            plugDir.mkdir();
         }
         /* Coming soon to a town near you... */
     }
---
+++ src/main/java/glucose/Logger.java
@@ -0,0 +1,7 @@
+package glucose;
+
+public class Logger {
+    public static <String> void info(String input){
+        System.out.println("[INFO] "+input);
+    }
+}
